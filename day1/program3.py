3e9aeb46ff8c7cf96884e9a0d741fbc53d5701f1
Pratyush-PS <60107386+Pratyush-PS@users.noreply.github.com>
2020-05-18 19:46:42 +0530

    Create program3.py

---
+++ day1/program3.py
@@ -0,0 +1,8 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+print("Initial Values are:",) 
+print("a=",a," b=",b)
+a,b=b,a
+print("Swapped Values are:",) 
+print("a=",a," b=",b)
