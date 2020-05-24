4f57aa982426ee2188ecbfc98fbe63d08cb876ce
Pratyush-PS <60107386+Pratyush-PS@users.noreply.github.com>
2020-05-24 18:54:04 +0530

    Create program11.py

---
+++ day6/program11.py
@@ -0,0 +1,11 @@
+size= int(input("\nEnter your list size:"))
+li=[]
+for i in range(size):
+    li.append(int(input("Enter element {}:".format(i+1))))
+
+print("\nGiven list is:",li)
+max_product=1
+for i in sorted(li)[-3:]:
+    max_product *=i 
+
+print("\nMaximum possible product is:",max_product)
