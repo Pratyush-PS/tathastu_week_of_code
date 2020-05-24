3f10e1b5ced4ff2211219cc9b2ce4bbb21ebe0ba
Pratyush-PS <60107386+Pratyush-PS@users.noreply.github.com>
2020-05-24 18:52:50 +0530

    Create program9.py

---
+++ day6/program9.py
@@ -0,0 +1,12 @@
+size = int(input("\nEnter size of your list:"))
+list=[]
+for i in range(size):
+    list.append(int(input("Enter element {}:".format(i+1))))
+
+print("\nThe given list is:",list)
+
+k= int(input("\nEnter value of k:"))
+
+sort = sorted(list)
+
+print("\nThe kth smallest value in list is=",sort[k-1])
