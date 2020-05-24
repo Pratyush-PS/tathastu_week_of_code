d765e48790b108b51a27c8eaaffcfc3ac47809f5
Pratyush-PS <60107386+Pratyush-PS@users.noreply.github.com>
2020-05-24 18:47:30 +0530

    Create program1.py

---
+++ day6/program1.py
@@ -0,0 +1,13 @@
+def make_palindrome(s):
+    if s==s[::-1]:
+        return 0
+    alpha={}
+    for i in s:
+        alpha[i] = alpha.get(i,0)+1
+    list = sorted(alpha.values())
+    odd_count = [i for i in list if i%2]
+    return(len(odd_count)-1)
+
+
+st1= input("Enter string:")
+print("Minimum deletions required:",make_palindrome(st1))         
